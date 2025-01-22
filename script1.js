95924a800965b5c400270cb0df35c6e04b1fdb85
hawhaw100 <hawraahalawi3@gmail.com>
2025-01-22 12:24:29 +0200

    last commit

--- script1.js
+++ script1.js
@@ -1,8 +1,27 @@
-document.querySelector("#show-login").addEventListener("click",function(){
-    document.querySelector(".popup").classList.add("active");
-})
-document.querySelector(".popup .close-btn").addEventListener("click",function(){
-    document.querySelector(".popup").classList.remove("active");
+const reviewform =document.getElementById('reviewform');
+const reviewlist =document.getElementById('reviewsList');
+const reviewbutton =document.getElementById('reviewbutton');
+
+reviewform.addEventListener('submit',function(event){
+   event.preventDefault();
+
+    const name =document.getElementById('name').value.trim();
+    const rating =document.getElementById('rating').value.trim();
+    const review =document.getElementById('review').value.trim();
+
+    console.log(name)
+    console.log(rating)
+    console.log(review)
+
+ const reviewItem= document.createElement('li');
+ reviewItem.innerHTML=`<strong>${name} </strong>
+  (Rating: ${rating}/5) <br> ${review}`;
+reviewlist.appendChild(reviewItem);
+reviewform.reset();
 })
 
+reviewbutton.addEventListener('click', function(){
+reviewform.reset();
+reviewlist.innerHTML ='';
+})
 
